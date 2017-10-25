25063dad50e5ef4f2bb0f3b8b0e5f1e6f440737c
Darshit Shah <darnir@gmail.com>
2017-10-25 14:55:09 +0200

    With commit 7c4ef44 bochs requires SDL. Hence build with SDL

--- src/misc/bochs-2.6.2-build.sh
+++ src/misc/bochs-2.6.2-build.sh
@@ -18,7 +18,8 @@ cat $PINTOSDIR/src/misc/bochs-2.6.2-jitter-plus-segv.patch | patch -p1
 cat $PINTOSDIR/src/misc/bochs-2.6.2-xrandr-pkgconfig.patch | patch -p1
 cat $PINTOSDIR/src/misc/bochs-2.6.2-banner-stderr.patch | patch -p1
 cat $PINTOSDIR/src/misc/bochs-2.6.2-block-device-check.patch | patch -p1
-CFGOPTS="--with-x --with-x11 --with-term --with-nogui --prefix=$DSTDIR"
+cat $PINTOSDIR/src/misc/bochs-2.6.2-link-tinfo.patch | patch -p1
+CFGOPTS="--with-x --with-x11 --with-term --with-nogui --with-sdl --prefix=$DSTDIR"
 mkdir plain &&
         cd plain && 
         ../configure $CFGOPTS --enable-gdb-stub && 
